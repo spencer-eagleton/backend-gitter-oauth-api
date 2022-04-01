d4e68663912dd387d8642bc04d64d598fc845a22
SPENCER EAGLETON <EAGLETONSPENCER@GMAIL.COM>
2022-04-01 11:24:15 -0700

    callback test is passing

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -3,6 +3,7 @@ const setup = require('../data/setup');
 const request = require('supertest');
 const app = require('../lib/app');
 
+jest.mock('../lib/utils/github');
 
 
 describe('backend-gitter-oauth-api routes', () => {
@@ -20,5 +21,15 @@ describe('backend-gitter-oauth-api routes', () => {
     expect(req.header.location).toMatch(
       /https:\/\/github.com\/login\/oauth\/authorize\?client_id=[\w\d]+&scope=user&redirect_uri=http:\/\/localhost:7890\/api\/v1\/github\/login\/callback/i
     );
+  });
+
+
+  it('should login and redirect users to /api/v1/github/', async () => {
+    const res = await request
+      .agent(app)
+      .get('/api/v1/github/login/callback?code=42')
+      .redirects(1);
+
+    expect(res.req.path).toEqual('/api/v1/posts');
   });
 });
