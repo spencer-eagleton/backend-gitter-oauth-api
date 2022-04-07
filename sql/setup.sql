73c93e93731b75d96ed2461d55f92e5db1525891
SPENCER EAGLETON <EAGLETONSPENCER@GMAIL.COM>
2022-04-07 12:10:25 -0700

    refactored user insert model

--- sql/setup.sql
+++ sql/setup.sql
@@ -1,6 +1,6 @@
 -- Use this file to define your SQL tables
 -- The SQL in this file will be executed when you run `npm run setup-db`
-DROP TABLE IF EXISTS github_users;
+DROP TABLE IF EXISTS github_users CASCADE;
 
 CREATE TABLE github_users (
   id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
@@ -9,7 +9,7 @@ CREATE TABLE github_users (
   avatar TEXT
 );
 
-DROP TABLE IF EXISTS posts;
+DROP TABLE IF EXISTS posts CASCADE;
 
 CREATE TABLE posts (
     id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
